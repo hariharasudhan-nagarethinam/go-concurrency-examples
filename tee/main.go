66d53f98e78235b4ddd16db4f8a01ddf9394089a
Hariharasudhan <hari@nineleaps.com>
2024-07-17 08:25:55 +0530

    tee channel.

--- tee/main.go
+++ tee/main.go
@@ -3,36 +3,30 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func orDone(done <-chan interface{}, c <-chan interface{}) <-chan interface{} {
-	valueStream := make(chan interface{})
+func randInd(done <-chan interface{}) <-chan interface{} {
+	intStream := make(chan interface{})
 
 	go func() {
-		defer close(valueStream)
+		defer close(intStream)
 
 		for {
+			time.Sleep(1 * time.Second)
 			select {
 			case <-done:
 				return
-			case val, ok := <-c:
-				if !ok {
-					return
-				}
-				select {
-				case <-done:
-					return
-				case valueStream <- val:
-				}
+			case intStream <- rand.Int():
 			}
 		}
 	}()
 
-	return valueStream
+	return intStream
 }
 
-func tee(done <-chan interface{}, c <-chan interface{}) (<-chan interface{}, <-chan interface{}) {
+func tee(done <-chan interface{}, in <-chan interface{}) (_, _ <-chan interface{}) {
 	out1 := make(chan interface{})
 	out2 := make(chan interface{})
 
@@ -40,17 +34,25 @@ func tee(done <-chan interface{}, c <-chan interface{}) (<-chan interface{}, <-c
 		defer close(out1)
 		defer close(out2)
 
-		for val := range orDone(done, c) {
-			var out1, out2 = out1, out2
-
-			for i := 0; i < 2; i++ {
-				select {
-				case <-done:
-					return
-				case out1 <- val:
-					out1 = nil
-				case out2 <- val:
-					out2 = nil
+		for {
+			select {
+			case <-done:
+				return
+			case val := <-in:
+				var out1, out2 = out1, out2
+				for i := 0; i < 2; i++ {
+					select {
+					case <-done:
+						return
+					case out1 <- val:
+						fmt.Println("matched out1")
+						out1 = nil
+					case out2 <- val:
+						fmt.Println("matched out2")
+						out2 = nil
+					default:
+						fmt.Println("default")
+					}
 				}
 			}
 		}
@@ -60,24 +62,34 @@ func tee(done <-chan interface{}, c <-chan interface{}) (<-chan interface{}, <-c
 }
 
 func main() {
+	wg := sync.WaitGroup{}
 	done := make(chan interface{})
-	defer close(done)
 
-	randInt := func() <-chan interface{} {
-		randStream := make(chan interface{})
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(done)
+		time.Sleep(2 * time.Second)
+	}()
 
-		go func() {
-			defer close(randStream)
+	out1, out2 := tee(done, randInd(done))
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(out1)
+		for val := range out1 {
+			fmt.Println("out1", val)
+		}
+	}()
 
-			time.Sleep(1 * time.Second)
-			randStream <- rand.Int()
-		}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for val := range out2 {
+			fmt.Println("out2", val)
+		}
+	}()
 
-		return randStream
-	}
+	wg.Wait()
 
-	out1, out2 := tee(done, randInt())
-	for val1 := range out1 {
-		fmt.Printf("out1: %v, out2: %v\n", val1, <-out2)
-	}
 }
